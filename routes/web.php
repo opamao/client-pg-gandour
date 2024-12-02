43b0575b127c5939dd90bcb2e61c65aa8a2d26f6
Kadhel AMAN <kadhelaman@192.168.1.3>
2024-12-02 19:57:22 +0000

    design

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,15 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
+});
+
+Route::get('index', function () {
+    return view('dashboard.dashboard');
+});
+Route::get('clients', function () {
+    return view('clients.clients');
+});
+Route::get('divisions', function () {
+    return view('divisions.divisions');
 });
